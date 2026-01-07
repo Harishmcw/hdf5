07f0bd88b02391b8d25488f7364b12ce36c7880a
Harish RS <harish.rajaselvan@multicorewareinc.com>
2026-01-07 18:00:33 +0530

    fix

--- config/CacheURLs.cmake
+++ config/CacheURLs.cmake
@@ -58,8 +58,8 @@ mark_as_advanced (KWSYS_TGZ_NAME)
 # filter plugin options
 ########################
 
-set (PLUGIN_TGZ_ORIGPATH "https://github.com/HDFGroup/hdf5_plugins/releases/download/snapshot" CACHE STRING "Use PLUGINS from original location")
-set (PLUGIN_TGZ_NAME "hdf5_plugins-master.tar.gz" CACHE STRING "Use PLUGINS from compressed file")
+set (PLUGIN_TGZ_ORIGPATH "https://github.com/Harishmcw/hdf5_plugins/releases/download/snapshot")
+set (PLUGIN_TGZ_NAME "hdf5_plugins-master.tar.gz")
 mark_as_advanced (PLUGIN_TGZ_ORIGPATH)
 mark_as_advanced (PLUGIN_TGZ_NAME)
 set (PLUGIN_PACKAGE_NAME "pl" CACHE STRING "Name of PLUGIN package")
